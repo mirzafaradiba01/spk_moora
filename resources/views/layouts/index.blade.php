da1529366a5c6b10aa775a55f8a4eba96f3f09a7
tesyaeriana <2141720024@student.polinema.ac.id>
2023-12-04 18:15:23 +0700

    fix_layouts

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,71 +1,57 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistem Pendukung Keputusan</title>
 
-  <title>SPK MOORA</title>
+    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('assets/icon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/icon/programming.png')}}">
+    <link rel="manifest" href="{{asset('/assets/icon/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('assets/icon/safari-pinned-tab.svg')}}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
+
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Bootstrap-->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <!-- Datatables-->
+    <link rel="stylesheet" href="{{asset('assets/css/datatables.min.css')}}" />
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
+
 </head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
 
-@include('layouts/navbar')
-@include('layouts/sidebar')
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        @include('layouts.navbar')
+        @include('layouts.sidebar')
+        @yield('content')
+        @include('layouts.footer')
 
-  <!-- Content Wrapper. Contains page content -->
- 
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    @yield('content')
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
     </div>
-  </aside>
-  <!-- /.control-sidebar -->
-@include('layouts/footer')
-  <!-- Main Footer -->
-  
-</div>
-<!-- ./wrapper -->
+    <!-- ./wrapper -->
 
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('assets/dist/js/adminlte.min.js')}}"></script>
-
-<script>
-  $.ajaxSetup({
-      headers: {
-          'X-CSRF-TOKEN': '{{csrf_token()}}'
-      }
-  }); 
-</script>
-
-@stack('js')
+    <!-- JQuery-->
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+    <!-- Bootstrap-->
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- Datatables-->
+    <script src="{{asset('assets/js/datatables.min.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('assets/dist/js/adminlte.js')}}"></script>
+    @yield('script')
 </body>
+
 </html>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,27 +2,19 @@
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('assets\img\polinema_logo.png') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('assets/img/logo.png') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a class="d-block">SPK/MOORA</a>
+                <a class="d-block">SPK</a>
             </div>
         </div>
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a href="{{ url('dashboard') }}" class="nav-link">
+                    <a href="{{ url('home') }}" class="nav-link">
                         <i class="nav-icon fas fa-home"></i>
                         <p>
                             Home
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('kriteriabobot') }}" class="nav-link">
-                        <i class="nav-icon fas fa-cube"></i>
-                        <p>
-                            Kriteria & Bobot
                         </p>
                     </a>
                 </li>
@@ -35,14 +27,46 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ url('datapenilaian') }}" class="nav-link">
+                    <a href="{{ url('kriteriabobot') }}" class="nav-link">
                         <i class="nav-icon fas fa-cube"></i>
                         <p>
-                            Data Penilaian
+                            Kriteria & Bobot
                         </p>
                     </a>
                 </li>
-               
+                <li class="nav-item">
+                    <a href="{{ url('penilaian') }}" class="nav-link">
+                        <i class="nav-icon fas fa-database"></i>
+                        <p>
+                            Penilaian
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-header">Hasil</li>
+                <li class="nav-item">
+                    <a href="{{ url('decision') }}" class="nav-link">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            Matriks Keputusan
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ url('normalization') }}" class="nav-link">
+                        <i class="nav-icon far fa-chart-bar"></i>
+                        <p>
+                            Normalisasi
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ url('rank') }}" class="nav-link">
+                        <i class="nav-icon fas fa-chart-line"></i>
+                        <p>
+                            Ranking
+                        </p>
+                    </a>
+                </li>
             </ul>
         </nav>
     </div>
